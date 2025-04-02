d14424f4aa225ee458e7d7d0899209699943f7e9
Gage Messner <gagemess8@gmail.com>
2025-04-01 18:29:04 -0700

    working on clickable workout cards now

--- src/app/new-workout/page.jsx
+++ src/app/new-workout/page.jsx
@@ -10,4 +10,3 @@ export default function NewWorkout() {
 
 }
 
-    return()
--- src/components/workoutCard.js
+++ src/components/workoutCard.js
@@ -2,12 +2,20 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+import { useRouter } from "next/navigation";
 
 
 export default function WorkoutCard({workout}) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Workout clicked:", workout);
+    router.push(`/workout/${workout.id}`);
+  };
+
     return (
       <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
+        <CardActionArea onClick={handleClick}>
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               {workout.type}
